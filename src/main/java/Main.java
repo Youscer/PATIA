9b23986495063a68176b6f8842829e7fa199dd0f
Youcef <khelil.youcef1@gmail.com>
2021-05-02 22:04:30 +0200

    Added JitPack dependency and Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import fr.uga.pddl4j.planners.ProblemFactory;
+
+public class Main {
+
+	public static void main(String[] args) {
+		final ProblemFactory factory = ProblemFactory.getInstance();
+	}
+
+}
